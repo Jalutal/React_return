afd4dbf009963cd502c5a3ff36a9e1d9e7a2bff2
Jesse Granier <jesse.granier@lapiscine.pro>
2023-12-15 12:21:42 +0100

    3 - modifs

--- src/pages/Cocktails.jsx
+++ src/pages/Cocktails.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 
